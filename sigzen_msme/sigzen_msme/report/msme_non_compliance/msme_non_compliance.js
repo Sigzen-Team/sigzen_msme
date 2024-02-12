504b66308eba68b76ee895ff7d01195417e56d7a
Jishnu-V <jishnuvv2016@gmail.com>
2024-02-12 16:10:58 +0530

    create new report MSME Non Compliance Report

---
+++ sigzen_msme/sigzen_msme/report/msme_non_compliance/msme_non_compliance.js
@@ -0,0 +1,87 @@
+// Copyright (c) 2024, Sigzen Msme and contributors
+// For license information, please see license.txt
+/* eslint-disable */
+
+frappe.query_reports["MSME Non-Compliance"] = {
+	"filters": [
+		{
+			"fieldname": "company",
+			"label": __("Company"),
+			"fieldtype": "Link",
+			"options": "Company",
+			"reqd": 1,
+			"default": frappe.defaults.get_user_default("Company")
+		},
+		{
+			"fieldname": "fiscal_year",
+			"label": __("Fiscal Year"),
+			"fieldtype": "Link",
+			"options": "Fiscal Year",
+			"default": erpnext.utils.get_fiscal_year(frappe.datetime.get_today()),
+			"reqd": 1,
+			"on_change": function(query_report) {
+				var fiscal_year = query_report.get_values().fiscal_year;
+				if (!fiscal_year) {
+					return;
+				}
+				frappe.model.with_doc("Fiscal Year", fiscal_year, function(r) {
+					var fy = frappe.model.get_doc("Fiscal Year", fiscal_year);
+					frappe.query_report.set_filter_value({
+						from_date: fy.year_start_date,
+						to_date: fy.year_end_date
+					});
+				});
+			}
+		},
+		{
+			"fieldname": "from_date",
+			"label": __("From Date"),
+			"fieldtype": "Date",
+			"default": frappe.defaults.get_user_default("year_start_date"),
+			"hidden": 1,
+		},
+		{
+			"fieldname": "to_date",
+			"label": __("To Date"),
+			"fieldtype": "Date",
+			"default": frappe.defaults.get_user_default("year_end_date"),
+			"hidden": 1,
+		},
+		{
+			"fieldname": "supplier",
+			"label": __("Supplier"),
+			"fieldtype": "Link",
+			"options": "Supplier",
+		},
+		{
+			"fieldname": "supplier_group",
+			"label": __("Supplier Group"),
+			"fieldtype": "Link",
+			"options": "Supplier Group",
+		},
+		{
+			"fieldname": "ageing_based_on",
+			"label": __("Ageing Based On"),
+			"fieldtype": "Select",
+			"options": 'Posting Date\nSupplier Invoice Date',
+			"default": "Posting Date"
+		},
+		{
+			"fieldname": "custom_msme_type",
+			"label": __("MSME Type"),
+			"fieldtype": "Select",
+			"options": '\nMicro\nSmall\nMedium',
+			
+		},
+		{
+			"fieldname": "custom_contract_done",
+			"label": __("MSME Contract Done"),
+			"fieldtype": "Select",
+			"options": '\nYes\nNo',
+			
+		},
+		
+		
+
+	],
+};
