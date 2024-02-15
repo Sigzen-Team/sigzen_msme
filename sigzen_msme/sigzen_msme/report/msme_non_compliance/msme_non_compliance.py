596daf8c1871545b2c1457cd34f2ce2785341cb0
Jishnu-V <jishnuvv2016@gmail.com>
2024-02-15 16:01:38 +0530

    update msme report

--- sigzen_msme/sigzen_msme/report/msme_non_compliance/msme_non_compliance.py
+++ sigzen_msme/sigzen_msme/report/msme_non_compliance/msme_non_compliance.py
@@ -1,6 +1,7 @@
 # Copyright (c) 2024, Sigzen Msme and contributors
 # For license information, please see license.txt
 
+from erpnext.accounts.doctype import journal_entry
 import frappe
 from frappe import _, qb, scrub
 import datetime
@@ -8,29 +9,90 @@ from frappe.utils import *
 
 
 def execute(filters=None):
+    data = get_data(filters)
+
     columns = [
-        {"label": "Purchase ID", "fieldname": "purchase_id", "fieldtype": "Link", "options": "Purchase Invoice", "width": 200},#// nosemgrep
-        {"label": "Supplier", "fieldname": "supplier", "fieldtype": "Link", "options": "Supplier", "width": 180},#// nosemgrep
-        {"label": "Supplier No", "fieldname": "supplier_no", "fieldtype": "Data", "width": 180},#// nosemgrep
-        {"label": "MSME Contract Done", "fieldname": "contract_yes_no", "fieldtype": "Data", "width": 170},#// nosemgrep
+        {
+            "fieldname": "purchase_id",
+            "label": _("Purchase ID"),
+            "fieldtype": "Link",
+            "options": "Purchase Invoice",
+            "width": 200
+        },
+        {
+            "fieldname": "supplier",
+            "label": _("Supplier"),
+            "fieldtype": "Link",
+            "options": "Supplier",
+            "width": 180
+        },
+        {
+            "fieldname": "supplier_no",
+            "label": _("Supplier No"),
+            "fieldtype": "Data",
+            "width": 180
+        },
+        {
+            "fieldname": "contract_yes_no",
+            "label": _("MSME Contract Done"),
+            "fieldtype": "Data",
+            "width": 170
+        }
     ]
+
     if filters.ageing_based_on == "Posting Date":
-        columns.append({"label": "Posting Date", "fieldname": "posting_date", "fieldtype": "Date", "width": 180})#// nosemgrep
+        columns.append({
+            "fieldname": "posting_date",
+            "label": _("Posting Date"),
+            "fieldtype": "Date",
+            "width": 180
+        })
     else:
-        columns.append({"label": "Supplier Invoice Date", "fieldname": "bill_date", "fieldtype": "Date", "width": 180})#// nosemgrep
+        columns.append({
+            "fieldname": "bill_date",
+            "label": _("Supplier Invoice Date"),
+            "fieldtype": "Date",
+            "width": 180
+        })
 
     columns.extend([
-        {"label": "Due Date", "fieldname": "due_date", "fieldtype": "Date", "width": 180},#// nosemgrep
-        {"label": "Invoice Amount", "fieldname": "invoice_amount", "fieldtype": "Currency", "width": 180},#// nosemgrep
-        {"label": "Current Outstanding", "fieldname": "outstanding", "fieldtype": "Currency", "width": 180},#// nosemgrep
-        {"label": "Paid Amt Before Due Dt", "fieldname": "paid_amount_before", "fieldtype": "Currency", "width": 180},#// nosemgrep
-        {"label": "Paid Amt After Due Dt", "fieldname": "paid_amount_after", "fieldtype": "Currency", "width": 180},#// nosemgrep
-        {"label": "Disallowed Amount", "fieldname": "disallowed_amount", "fieldtype": "Currency","width": 180}#// nosemgrep
-    ]) 
-
-    
-
-    data = get_data(filters)
+        {
+            "fieldname": "due_date",
+            "label": _("Due Date"),
+            "fieldtype": "Date",
+            "width": 180
+        },
+        {
+            "fieldname": "invoice_amount",
+            "label": _("Invoice Amount"),
+            "fieldtype": "Currency",
+            "width": 180
+        },
+        {
+            "fieldname": "outstanding",
+            "label": _("Current Outstanding"),
+            "fieldtype": "Currency",
+            "width": 180
+        },
+        {
+            "fieldname": "paid_amount_before",
+            "label": _("Paid Amt Before Due Dt"),
+            "fieldtype": "Currency",
+            "width": 180
+        },
+        {
+            "fieldname": "paid_amount_after",
+            "label": _("Paid Amt After Due Dt"),
+            "fieldtype": "Currency",
+            "width": 180
+        },
+        {
+            "fieldname": "disallowed_amount",
+            "label": _("Disallowed Amount"),
+            "fieldtype": "Currency",
+            "width": 180
+        }
+    ])
     return columns, data
 
 def get_data(filters):
@@ -45,7 +107,7 @@ def get_data(filters):
             `tabSupplier`.custom_msme_registered,
             `tabSupplier`.custom_msme_type,
             `tabPurchase Invoice`.posting_date,
-            `tabPurchase Invoice`.base_grand_total,
+            `tabPurchase Invoice`.base_rounded_total,
             `tabPurchase Invoice`.outstanding_amount,
             `tabPurchase Invoice`.status,
             `tabPurchase Invoice`.bill_no,
@@ -54,9 +116,10 @@ def get_data(filters):
             `tabSupplier`,
             `tabPurchase Invoice`
         WHERE
+            `tabPurchase Invoice`.posting_date between %(from_date)s AND %(to_date)s AND
             `tabPurchase Invoice`.supplier = `tabSupplier`.name AND
-            `tabPurchase Invoice`.status NOT IN ('Cancelled', 'Draft', 'Return', 'Debit Note Issued') AND
-            `tabPurchase Invoice`.posting_date between %(from_date)s AND %(to_date)s 
+            `tabPurchase Invoice`.status NOT IN ('Cancelled', 'Draft', 'Return', 'Debit Note Issued')
+            
         
             
     """
@@ -107,21 +170,31 @@ def get_data(filters):
         disallowed_amount = 0
 
         payment_entries = frappe.get_all("Payment Entry", filters={"reference_name": invoice.name}, fields=["posting_date","paid_amount"])
+        journal_entry = frappe.get_all("Journal Entry", filters={"reference_name": invoice.name}, fields=["posting_date","total_debit"])
+        if payment_entries:
+            for entry in payment_entries:
+                posting_date = entry.get("posting_date")
 
-        for entry in payment_entries:
-            posting_date = entry.get("posting_date")
+                if posting_date > due_date:
+                    paid_amount_after += entry.get("paid_amount")
+                    disallowed_amount += entry.get("paid_amount")  
+                else:
+                    paid_amount_before += entry.get("paid_amount")
+        else:
+            for entry in journal_entry:
+                posting_date = entry.get("posting_date")
 
-            if posting_date > due_date:
-                paid_amount_after += entry.get("paid_amount")
-                disallowed_amount += entry.get("paid_amount")  
-            else:
-                paid_amount_before += entry.get("paid_amount")  
+                if posting_date > due_date:
+                    paid_amount_after += entry.get("total_debit")
+                    disallowed_amount += entry.get("total_debit")  
+                else:
+                    paid_amount_before += entry.get("total_debit")  
 
-        if invoice.custom_msme_registered == "Yes" and invoice.base_grand_total != paid_amount_before and invoice.custom_msme_type != "Medium":
+        if invoice.custom_msme_registered == "Yes" and invoice.base_rounded_total != paid_amount_before and invoice.custom_msme_type != "Medium":
             row = {
                 'purchase_id': invoice.name,
                 'supplier': invoice.supplier,
-                'invoice_amount': invoice.base_grand_total,
+                'invoice_amount': invoice.base_rounded_total,
                 'contract_yes_no': invoice.custom_contract_done,
                 'posting_date': invoice.posting_date,
                 'due_date': due_date,
