2fd1e2060a36ca752079ca100996da7b5c0595ce
Ami-sigzen <ami.t@sigzen.com>
2025-04-08 11:44:33 +0530

    Revert "Feat: Add custom fields in the Address"
    This reverts commit ac7ab2e21a2bbce5f157c14ef8a064be54145272

--- sigzen_msme/constants/custom_fields.py
+++ sigzen_msme/constants/custom_fields.py
@@ -15,31 +15,5 @@ b= {
             "label": "MSME Details",
             "is_system_generated": 0
         }
-    ],
-    "Address": [
-        {
-            "fieldname": "custom_msme_details_sections",
-            "fieldtype": "Section Break",
-            "insert_after": "custom_iso_remark",
-            "label": "MSME Details",
-            "collapsible": 1,
-            "is_system_generated": 0 
-        },
-        {
-            "fieldname": "custom_is_msme",
-            "fieldtype": "Check",
-            "insert_after": "custom_msme_details_sections",
-            "label": "Is MSME",
-            "is_system_generated": 0
-        }, 
-        {
-            "fieldname": "custom_msme_details",
-            "fieldtype": "Table",
-            "options" :  "MSME Details",
-            "insert_after": "custom_is_msme",
-            "depends_on": "eval:doc.custom_is_msme;",
-            "label": "MSME Details",
-            "is_system_generated": 0
-        }
     ]
 }
